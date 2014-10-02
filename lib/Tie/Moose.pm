03fc6c105dd72221cb27c568a5b450a88608f2a7
Toby Inkster <mail@tobyinkster.co.uk>
2014-10-02 23:22:45 +0100

    bump version; changelog

--- lib/Tie/Moose.pm
+++ lib/Tie/Moose.pm
@@ -5,7 +5,7 @@ use warnings;
 package Tie::Moose;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.002';
+our $VERSION   = '0.003';
 
 use Moose;
 use namespace::autoclean;
--- lib/Tie/Moose/Forgiving.pm
+++ lib/Tie/Moose/Forgiving.pm
@@ -5,7 +5,7 @@ use warnings;
 package Tie::Moose::Forgiving;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.002';
+our $VERSION   = '0.003';
 
 use Moose::Role;
 use namespace::autoclean;
